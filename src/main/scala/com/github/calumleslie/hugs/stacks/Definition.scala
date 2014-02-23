9f41fff0e259be1808c96c76b91204cceb5ed9ca
Calum Leslie <calumleslie@gmail.com>
2014-02-23 16:07:45 +0000

    Make it possible to add documentation to word definitions.

--- src/main/scala/com/github/calumleslie/hugs/stacks/Definition.scala
+++ src/main/scala/com/github/calumleslie/hugs/stacks/Definition.scala
@@ -4,11 +4,18 @@ import com.github.calumleslie.hugs.stacks.lang.Particle
 
 trait Definition {
   def apply(state: State): State
+  def documentation = "(no documentation)"
 }
 
 object Definition {
   def apply(transform: State => State) = new Definition {
     def apply(state: State) = transform(state)
+    override def toString() = "(native)"
+  }
+
+  def apply(docs: String, transform: State => State) = new Definition {
+    def apply(state: State) = transform(state)
+    override def documentation = docs
     override def toString() = "(native)"
   }
 }
--- src/main/scala/com/github/calumleslie/hugs/stacks/PureDefinition.scala
+++ src/main/scala/com/github/calumleslie/hugs/stacks/PureDefinition.scala
@@ -4,13 +4,19 @@ import com.github.calumleslie.hugs.stacks.lang.Quotation
 import com.github.calumleslie.hugs.stacks.lang.Particle
 import com.github.calumleslie.hugs.stacks.lang.Quotation
 
-class PureDefinition(val definition: Seq[Particle]) extends Definition {
+class PureDefinition(docs: String, val definition: Seq[Particle]) extends Definition {
   def apply(state: State) = state.withCallstack(List(definition: _*) :: state.callStack)
 
+  def withDocumentation(newDocs: String) = new PureDefinition(newDocs, definition)
+
+  override def documentation = docs
   override def toString() = definition.mkString(" ")
 }
 
 object PureDefinition {
-  def apply(quotation: Quotation): PureDefinition = new PureDefinition(quotation.particles)
-  def apply(code: String): PureDefinition = new PureDefinition(Parser.parse(code))
+  def apply(definition: Seq[Particle]): PureDefinition = new PureDefinition("(no documentation)", definition)
+  def apply(quotation: Quotation): PureDefinition = apply(quotation.particles)
+  def apply(code: String): PureDefinition = apply(Parser.parse(code))
+  def apply(documentation: String, quotation: Quotation): PureDefinition = new PureDefinition(documentation, quotation.particles)
+  def apply(documentation: String, code: String): PureDefinition = new PureDefinition(documentation, Parser.parse(code))
 }
